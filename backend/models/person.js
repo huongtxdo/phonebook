4879d15e8574a0c8f80a94e08b2ef9524b897d8f
huongtxdo <huong.tx.do@gmail.com>
2023-06-24 02:55:12 +0300

    fix all ESlint warnings

--- backend/models/person.js
+++ backend/models/person.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
-console.log("connecting to", url);
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
-  .then((result) => console.log("connected to MongoDB"))
-  .catch((error) => console.log("error connecting to MongoDB:", error.message));
+  .then(() => console.log('connected to MongoDB'))
+  .catch((error) => console.log('error connecting to MongoDB:', error.message))
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -18,25 +18,25 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: [9, `Must be at least 8 numbers with 1 dash ("-")`],
+    minLength: [9, 'Must be at least 8 numbers with 1 dash ("-")'],
     validate: [
       {
         validator: function (v) {
-          return /\d{3}-\d{5}/.test(v) || /\d{2}-\d{6}/.test(v);
+          return /\d{3}-\d{5}/.test(v) || /\d{2}-\d{6}/.test(v)
         },
         message:
-          "Only number and number format should be 123-45678... or 12-345678...",
+          'Only number and number format should be 123-45678... or 12-345678...',
       },
     ],
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
