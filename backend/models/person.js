a9f47ee1ec7197b2ea7f0d1924461fa241c419b5
huongtxdo <huong.tx.do@gmail.com>
2023-06-24 01:17:17 +0300

    delete unnecessary things

--- backend/models/person.js
+++ backend/models/person.js
@@ -39,24 +39,4 @@ personSchema.set("toJSON", {
   },
 });
 
-// if (process.argv.length == 3) {
-//   Person.find({}).then((persons) => {
-//     console.log("phonebook:");
-//     persons.forEach((person) => console.log(`${person.name} ${person.number}`));
-//     mongoose.connection.close();
-//   });
-// } else if (process.argv.length == 5) {
-//   const name = process.argv[3];
-//   const number = process.argv[4];
-//   const person = new Person({
-//     name: name,
-//     number: number,
-//   });
-
-//   person.save().then((result) => {
-//     console.log(`added ${result.name} number ${result.number} to phonebook`);
-//     mongoose.connection.close();
-//   });
-// }
-
 module.exports = mongoose.model("Person", personSchema);
